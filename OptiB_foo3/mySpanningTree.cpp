98b2ecaa93d3cc2e9c68827f73930dbea5cf9c68
Tobin Holtmann <9052510+Crymp@users.noreply.github.com>
2022-11-08 10:59:13 +0100

    MST working

--- OptiB_foo3/mySpanningTree.cpp
+++ OptiB_foo3/mySpanningTree.cpp
@@ -30,6 +30,12 @@ void iterate_nodes(const Graph& g, std::unordered_map<int, int> colour_of_node) 
 
 }
 
+int get_number_of_nodes(const Graph& g) {
+    graph_traits<Graph>::vertex_iterator node_iter_begin, node_iter_end;
+    tie(node_iter_begin, node_iter_end) = vertices(g);
+    int n = node_iter_end - node_iter_begin;
+    return n;
+}
 
 
 std::vector<Edge> my_spanning_tree(const Graph& g) {
@@ -38,27 +44,31 @@ std::vector<Edge> my_spanning_tree(const Graph& g) {
 
         // ...
 
-    // Get list of edges in a way such that it can be sorted by stl
-    //graph_traits<Graph>::edge_iterator edges_it_start, edges_it_end;
-    //tie(edges_it_start, edges_it_end) = edges(g);
     
-    
+    // Sort all edges by weight
+    // Because boosts' edge_iterator is not working with stl's sort, first awkardly copy the edges into a stl vector and then use that to sort...
     std::vector<Edge> sorted_edges;
     graph_traits<Graph>::edge_iterator edge_it_start, edges_it_end;
     tie(edge_it_start, edges_it_end) = edges(g);
-
     for (auto edge_it = edge_it_start; edge_it != edges_it_end; edge_it++) {
         sorted_edges.push_back(*edge_it);
     }
-    
-    
-    // Sorted Edges
+    // Sort
     std::sort(sorted_edges.begin(), sorted_edges.end(),
         [g](Edge e1, Edge e2) -> bool {return (get(edge_weight, g, e1)) < get(edge_weight, g, e2); }
     );
 
     
-    // Loop over edges until complete
+    int num_of_nodes = get_number_of_nodes(g);
+
+    // Now add edges in increasing order of weight, rejecting them if they close a loop
+    // 
+    // To keep if an edge closes a loop, each node is assigned a 'colour', all nodes that are connected (ie connected components) share the same colour
+    // If an edge joins two connected components, the colour of one connected components is overriden
+    //
+    // To keep track of what colour of a node is, and which nodes share the same colour: using two hashmaps
+    // Rather than hash the boost node object itself, just using the int index of each vertex
+
     std::unordered_map<int, int> colour_of_node; //node->colour    returns the colour of a node
     std::unordered_map<int, std::vector<int>> nodes_with_colour; //colour->node  returns list of node that have colour
     int max_colour = -1;
@@ -75,7 +85,7 @@ std::vector<Edge> my_spanning_tree(const Graph& g) {
         int colour2 = (colour_of_node.find(node2) == colour_of_node.end()) ? -2 : colour_of_node[node2];
         // -2 signifies has never been found yet
 
-        std::cout << get(edge_weight, g, *edge) << " - (" << node1 << ": " << colour1 << ", " << node2 << ": " << colour2 << " ) ";
+        //std::cout << get(edge_weight, g, *edge) << " - (" << node1 << ": " << colour1 << ", " << node2 << ": " << colour2 << " ) ";
         if (colour1 == colour2) {
             if (colour1 == -2) {
                 // Both nodes have not been added yet
@@ -83,13 +93,13 @@ std::vector<Edge> my_spanning_tree(const Graph& g) {
                 colour_of_node[node1] = max_colour;
                 colour_of_node[node2] = max_colour;
                 nodes_with_colour[max_colour] = std::vector<int>{ node1, node2 };
-                std::cout << "both unseen nodes " << node1 << " & " << node2 << ", now with colour " << max_colour << std::endl;
+                //std::cout << "both unseen nodes " << node1 << " & " << node2 << ", now with colour " << max_colour << std::endl;
                 spanning_tree.push_back(*edge);
 
             }
             else {
                 // Both nodes same colour... this would close a loop. Skip this.
-                std::cout << "closed loop" << colour1 << std::endl;
+                //std::cout << "closed loop" << colour1 << std::endl;
             }
 
         }
@@ -98,7 +108,7 @@ std::vector<Edge> my_spanning_tree(const Graph& g) {
                 // Give node1 the same colour as node2
                 colour_of_node[node1] = colour2;
                 nodes_with_colour[colour2].push_back(node1);
-                std::cout << "node " << node1 << " unseen" << std::endl;
+                //std::cout << "node " << node1 << " unseen" << std::endl;
 
             }
             else if (colour2 == -2) {
@@ -109,18 +119,18 @@ std::vector<Edge> my_spanning_tree(const Graph& g) {
 
                //nodes_with_colour[colour1] = foo1;
                nodes_with_colour[colour1].push_back(node2);
-               std::cout << "node " << node2 <<" unseen" << std::endl;
+              // std::cout << "node " << node2 <<" unseen" << std::endl;
             }
             else {
                 // Pick node with lower colour (higher colour liklier to contain less nodes)
                 // Make all nodes that have the colour as node2 the same colour as node1
-                std::cout << " - repainting nodes with colour " << colour1 << ": ";
+                //std::cout << " - repainting nodes with colour " << colour1 << ": ";
                 std::vector<int> nodes_to_repaint = nodes_with_colour[colour2];
                 for (auto node = nodes_to_repaint.begin(); node != nodes_to_repaint.end(); node++) {
-                    std::cout << *node << ", ";
+                    //std::cout << *node << ", ";
                     colour_of_node[*node] = colour1;
                 }
-                std::cout << std::endl;
+               // std::cout << std::endl;
                 //Update entry of nodes that have colours 1
                 std::vector<int> nodes_with_colour1 = nodes_with_colour[colour1];
                 nodes_with_colour1.insert(nodes_with_colour1.end(), nodes_to_repaint.begin(), nodes_to_repaint.end());
@@ -134,7 +144,9 @@ std::vector<Edge> my_spanning_tree(const Graph& g) {
        }
         // ...
 
-        
+        if (spanning_tree.size() == num_of_nodes - 1) {
+            return spanning_tree;
+       }
     }
     //iterate_nodes(g, colour_of_node);
     return spanning_tree;
